4f403afb4470d0138edff35de0e517845d5f1943
michaelkunc <mjkunc@gmail.com>
2017-04-01 15:12:06 -0700

    start of test suite for album etl

--- tests/test_etl_album.py
+++ tests/test_etl_album.py
@@ -20,6 +20,12 @@ class Album_Test(unittest.TestCase):
         self.assertEqual([1, 1967, "Sgt. Pepper's Lonely Hearts Club Band", "The Beatles",
                           "Rock", "Rock & Roll, Psychedelic Rock"], list(Album_Test.data.df.ix[0]))
 
-    def test_max_min_years(self):
-        self.assertEqual((1955, 2011), list(
-            Album_Test.data.df['Year'].min()))
+    def test_min__max_years(self):
+        self.assertEqual((1955, 2011),
+                         tuple((int(Album_Test.data.df['Year'].min()), int(Album_Test.data.df['Year'].max()))))
+
+    def test_albums_by_year_shape(self):
+        self.assertEqual((56,), Album_Test.data.albums_by_year.shape)
+
+    def test_albums_by_year_first_row(self):
+        self.assertEqual('butt', list(Album_Test.data.albums_by_year))

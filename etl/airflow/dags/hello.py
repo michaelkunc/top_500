8fec92f56b8d7c910c16a6b10dfb2737a89f98da
michaelkunc <mjkunc@gmail.com>
2017-05-11 16:46:16 -0700

    think airflow is finally straightened out

--- etl/airflow/dags/hello.py
+++ etl/airflow/dags/hello.py
@@ -1,3 +1,7 @@
+"""
+Code that goes along with the Airflow tutorial located at:
+https://github.com/airbnb/airflow/blob/master/airflow/example_dags/tutorial.py
+"""
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
@@ -18,8 +22,7 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG(
-    'tutorial', default_args=default_args, schedule_interval=timedelta(1))
+dag = DAG('hello', default_args=default_args)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
